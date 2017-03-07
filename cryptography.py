bddeb75f97f3814dbdf6a1b5d0b02fe43e57fb07
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-03-07 14:42:39 -0500

    Updated from Brython Server: 3/7/2017 2:42:36 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jasper
+Credit: Brendan
 
 Assignment:
 
@@ -10,3 +10,34 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+i=list(input("Message: "))
+inp=[associations.find(x) for x in i]
+li=len(inp)
+k=list(input("Key: "))
+key=[associations.find(x) for x in k]
+lk=len(key)
+to=list('1'*li)
+j=0
+
+while j<lk:
+    to[j]=inp[j]+key[j]
+    print (to[j])
+    j+1
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
