396d73a4cf84a521805bafa13bbb9b4bc8830080
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-03-08 13:45:52 -0500

    Updated from Brython Server: 3/8/2017 1:45:51 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
 Author: Jasper
-Credit: Brendan
+Credit: Brendan, Ethan
 
 Assignment:
 
@@ -9,7 +9,7 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 i=list(input("Message: "))
 inp=[associations.find(x) for x in i]
 li=len(inp)
@@ -18,18 +18,12 @@ key=[associations.find(x) for x in k]
 lk=len(key)
 to=list('1'*li)
 j=0
-p=0
 
-while p<li:
-    to[j]=int(inp[p])+int(key[j])
+while j<li:
+    to[j]=int(inp[j])+int(key[j%(lk-1)])
     j=j+1
-    p=p+1
-    if j==lk-1:
-        j=0
 
-
-
-print (to)
+to=(associations[(to)])
 
 
 
