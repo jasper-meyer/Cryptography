d6d4956cddd6aa8c7ef94682736cee7b5cef2567
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-03-08 14:01:32 -0500

    Updated from Brython Server: 3/8/2017 2:01:30 PM

--- cryptography.py
+++ cryptography.py
@@ -10,20 +10,35 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+what =input( "Enter e to encrypt, d to decrypt, or q to quit: ")
 i=list(input("Message: "))
 inp=[associations.find(x) for x in i]
 li=len(inp)
+dinp=[associations.find(x) for x in i]
 k=list(input("Key: "))
 key=[associations.find(x) for x in k]
 lk=len(key)
 to=list('1'*li)
+tot=list('1'*li)
 j=0
 
-while j<li:
-    to[j]=int(inp[j])+int(key[j%(lk-1)])
-    j=j+1
 
-to=(associations[(to)])
+if what == "e":
+    while j<li:
+        to[j]=int(inp[j])+int(key[j%(lk-1)])
+        j=j+1
+     
+        to=[associations[x] for x in to]
+        j=0
+
+
+if what == "d":
+    while j<li:
+        tot[j]=int(inp[j])-int(key[j%(lk-1)])
+        j=j+1
+        
+        tot=[associations[x] for x in tot]
+
 
 
 
