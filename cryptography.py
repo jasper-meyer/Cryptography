02a0e7fbf0a21feb63cffb19b0ebbf861faf593f
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-03-10 14:47:16 -0500

    Updated from Brython Server: 3/10/2017 2:47:12 PM

--- cryptography.py
+++ cryptography.py
@@ -10,22 +10,22 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-what = 'a'
-what =input( "Enter e to encrypt, d to decrypt, or q to quit: ")
-if what !='q':
-    i=list(input("Message: "))
-    inp=[associations.find(x) for x in i]
-    li=len(inp)
-    dinp=[associations.find(x) for x in i]
-    k=list(input("Key: "))
-    key=[associations.find(x) for x in k]
-    lk=len(key)
-    to=list('1'*li)
-    tot=list('1'*li)
+
     
 while what!='q': 
     
+    what =input( "Enter e to encrypt, d to decrypt, or q to quit: ")
+
     if what == "e":
+        i=list(input("Message: "))
+        inp=[associations.find(x) for x in i]
+        li=len(inp)
+        dinp=[associations.find(x) for x in i]
+        k=list(input("Key: "))
+        key=[associations.find(x) for x in k]
+        lk=len(key)
+        to=list('1'*li)
+        tot=list('1'*li)
         j=0
         while j<li:
             to[j]=int(inp[j])+int(key[j%(lk-1)])
@@ -38,6 +38,15 @@ while what!='q':
     
     
     elif what == "d":
+        i=list(input("Message: "))
+        inp=[associations.find(x) for x in i]
+        li=len(inp)
+        dinp=[associations.find(x) for x in i]
+        k=list(input("Key: "))
+        key=[associations.find(x) for x in k]
+        lk=len(key)
+        to=list('1'*li)
+        tot=list('1'*li)
         j=0
         while j<li:
             tot[j]=int(inp[j])-int(key[j%(lk-1)])
